0fcf2d9094e44015a5986908f38ab6731eb5736e
Dinda Oktavia Randri <randri@mhs.unsyiah.ac.id>
2020-05-29 02:37:12 +0700

    commit contact us

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+class ContactController extends Controller
+{
+    public function showContactForm(){
+        return view('contact-form');
+    }
+
+    public function sendMail(Request $request){
+        
+        $subject = "Contact dari " . $request->input('name');
+        $name = $request->input('name');
+        $emailAddress = $request->input('email');
+        $message = $request->input('message');
+
+        $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+        try {
+            // Pengaturan Server
+           // $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+            $mail->isSMTP();                                      // Set mailer to use SMTP
+            $mail->Host = 'smtp.gmail.com';                  // Specify main and backup SMTP servers
+            $mail->SMTPAuth = true;                               // Enable SMTP authentication
+            $mail->Username = 'wallnote.rpl11@gmail.com';                 // SMTP username
+            $mail->Password = 'wallnote11';                           // SMTP password
+            $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+            $mail->Port = 465;                                    // TCP port to connect to
+
+            // Siapa yang mengirim email
+            $mail->setFrom("dindaoktavia0110@gmail.com", "Dinda Oktavia");
+
+            // Siapa yang akan menerima email
+            $mail->addAddress('wallnote.rpl11@gmail.com', 'Wallnote');     // Add a recipient
+            // $mail->addAddress('ellen@example.com');               // Name is optional
+
+            // ke siapa akan kita balas emailnya
+            $mail->addReplyTo($emailAddress, $name);
+            
+            // $mail->addCC('cc@example.com');
+            // $mail->addBCC('bcc@example.com');
+
+            //Attachments
+            // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+            // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+
+            //Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = $subject;
+            $mail->Body    = $message;
+            $mail->AltBody = $message;
+
+            $mail->send();
+
+            $request->session()->flash('status', 'Terima kasih, kami sudah menerima email anda.');
+            return view('contact-form');
+
+        } catch (Exception $e) {
+            echo 'Message could not be sent.';
+            echo 'Mailer Error: ' . $mail->ErrorInfo;
+        }
+
+    }
+}
